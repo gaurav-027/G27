82d997a3355b0cd0e85d3579858c7dce4b100fd8
Gaurav Kumar <kr.gauravbca7@gmail.com>
2026-08-28 23:02:40 +0530

    dvxv

--- Client/src/layout/Contact.jsx
+++ Client/src/layout/Contact.jsx
@@ -6,9 +6,13 @@ import Model from "../components/Model";
 export default function Contact() {
   return (
     <>
-      <div className="w-full h-screen bg-[#f2e2c2] overflow-hidden">
+      <div className="w-full h-screen bg-[#f2e2c2]">
         <div className="w-full h-full flex p-2 gap-2">
-          <div className="w-35/10 h-full bg-black rounded-2xl"></div>
+          <div className="w-35/10 h-full bg-black rounded-2xl p-5">
+            <div className="w-full border-b border-zinc-400 pb-2">
+                <p className="text-7xl text-right"> ✦ Contact</p>
+            </div>
+          </div>
           <div className="w-35/10 h-full rounded-2xl flex flex-col gap-2">
             <div className="model w-full h-8/1 bg-zinc-700 rounded-2xl flex justify-center items-center p-10">
                     <Model />
