544378801314e5f2c21f880e9629bb545aceed20
Gaurav Kumar <kr.gauravbca7@gmail.com>
2026-08-29 21:01:16 +0530

    vsdnns

--- Client/src/layout/Contact.jsx
+++ Client/src/layout/Contact.jsx
@@ -8,7 +8,7 @@ export default function Contact() {
     <>
       <div className="w-full h-screen bg-[#f2e2c2]">
         <div className="w-full h-full flex p-2 gap-2">
-          <div className="w-35/10 h-full bg-black rounded-2xl p-5">
+          <div className="contactForm w-35/10 h-full bg-black rounded-2xl p-5">
             <div className="w-full border-b border-zinc-400 pb-2">
                 <p className="text-7xl text-right"> ✦ Contact</p>
             </div>
